fc5529cb9d92254e022a293cd06096669c34ceac
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-04-18 00:54:27 -0300

    added todo button feature

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,56 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './styles/css/style.css';
-import Card from '../../components/Card';
 
-export default function Home() {
+export default () => {
+   const [toDos, setToDos] = useState([]);
+   const [inputValue, setInputValue] = useState('');
+
+   const toDoItems = toDos.map(toDo => {
+      return (
+         <li key={toDo.id}>
+            <p>{toDo.name}</p>
+            <div>
+               <button>
+                  <i className="bi bi-x-circle"></i>
+               </button>
+               <button>
+                  <i className="bi bi-pencil-square"></i>
+               </button>
+            </div>
+         </li>
+      );
+   });
+
+   const handleSubmit = e => {
+      e.preventDefault();
+
+      const toDo = {
+         id: toDos.length + 1,
+         name: inputValue,
+         isFinished: false,
+      };
+
+      setToDos(prevState => [...prevState, toDo]);
+      setInputValue('');
+   };
+
    return (
       <section className="to-do-app">
          <header>
             <h1> What's the Plan for Today? </h1>
          </header>
-         <form>
-            <input type="text" placeholder="Add a todo" />
+
+         <form className="form-main" onSubmit={handleSubmit}>
+            <input
+               value={inputValue}
+               onChange={e => setInputValue(e.target.value)}
+               type="text"
+               placeholder="Add a todo"
+            />
             <button type="submit">Add Todo</button>
          </form>
-         <ul>
-            <Card />
-         </ul>
+
+         <ul className="cards">{toDoItems}</ul>
       </section>
    );
-}
+};
